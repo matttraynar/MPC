0be5b907a02f5c90f3db3e4e57739b0758ffcfb7
Matt Traynar <matt.traynar@gmail.com>
2017-01-08 11:40:57 +0000

    Final commit before merge

--- python/voronoiExporter.py
+++ python/voronoiExporter.py
@@ -7,7 +7,7 @@ count = 0
 
 for node in nodes:
     cmds.select(node)
-    cmds.file("E:/mattt/Documents/MPC/objFiles/Voronoi/cubeFrag" + str(count)+".obj", op = "groups=0;ptgroups=0;materials=0;smoothing=0;normals=1", type = "OBJexport", es = True)
+    cmds.file("E:/mattt/Documents/MPC/objFiles/Voronoi2/cubeFrag" + str(count)+".obj", op = "groups=0;ptgroups=0;materials=0;smoothing=0;normals=1", type = "OBJexport", es = True)
     count += 1
 
 print nodes
